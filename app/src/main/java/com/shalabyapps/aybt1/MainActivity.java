d1013c0186116ac9fbf79d9e47426f68d47af41e
Abdelrahman Shalaby <abdo3bido@gmail.com>
2019-04-05 12:01:20 +0200

    Initial commit

---
+++ app/src/main/java/com/shalabyapps/aybt1/MainActivity.java
@@ -0,0 +1,25 @@
+package com.shalabyapps.aybt1;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void login(View v)
+    {
+        Intent intent = new Intent(getApplicationContext(),Main2Activity.class);
+        startActivity(intent);
+    }
+    public void signUp(View v)
+    {
+        Intent intent = new Intent(getApplicationContext(),Main3Activity.class);
+        startActivity(intent);
+    }
+}
