d0076d5c91f2f6db2042d349579089f0f70014db
Abdelrahman Shalaby <abdo3bido@gmail.com>
2019-04-27 15:29:27 +0200

    Minor Edits

--- app.js
+++ app.js
@@ -40,15 +40,22 @@ $(document).ready(function(){
         var flag = 0;
         var urls =[];
         for(var x=0;x<pic.files.length;x++){
+	    var d = new Date();
+            var _date = d.getTime();
+            var mime = "";
+            if(pic.files[x].name.split(".").length>0){
+                mime = "." + pic.files[x].name.split(".")[1];
+            }
+	        var name = _date.toString()+mime;
             var storageRef = storage.ref();
-            var picRef = storageRef.child(pic.files[x].name); 
+            var picRef = storageRef.child(name); 
             picRef.put(pic.files[x]).then(function(snapshot){
                 if(snapshot.state!="success"){
                     flag = 1;
                 }
             });
             
-            urls.push(pic.files[x].name);
+            urls.push(name);
         }
         if(flag == 0){
             firebaseWrite(urls);
@@ -60,38 +67,3 @@ $(document).ready(function(){
     
 }
 });
-
-//function write()
-//{
-//    // Initialize Firebase
-//      var config = {
-//        apiKey: "AIzaSyCCOjnLbML2FXx-YRcK7_uVORWSMYmtFAc",
-//        authDomain: "ayb1-b0261.firebaseapp.com",
-//        databaseURL: "https://ayb1-b0261.firebaseio.com",
-//        projectId: "ayb1-b0261",
-//        storageBucket: "ayb1-b0261.appspot.com",
-//        messagingSenderId: "700631630670"
-//      };
-//      firebase.initializeApp(config);
-//
-//    var database = firebase.database();
-//    title = document.getElementById("title").value;
-//    caseType = document.getElementById("caseType").value;
-//    description = document.getElementById("description").value;
-//    alert("HelloWorld");
-//    var _case = {
-//        Title: title,
-//        CaseType: caseType,
-//        Description: description
-//    };
-//    var newPostKey = firebase.database().ref().child('cases').push().key;
-//    var update={};
-//    update['/cases/'+newPostKey]= _case;
-//    firebase.database().ref().update(update);
-//    alert("Done");
-//  }
-//$document.ready(function(){
-//    $("#submit").click(function(){
-//        
-//    });
-//})
